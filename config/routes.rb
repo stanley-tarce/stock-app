a47c9b74aca505b3df6dbf6c71d11b9936d844e0
Stanley Tarce <stanleytarce18@gmail.com>
2021-12-15 15:22:08 +0800

    Deleted Admin Roles

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-
-  mount_devise_token_auth_for 'Admin', at: 'admin_auth'
-  as :admin do
-    # Define routes for Admin within this block.
-
-    post 'admin/create_user' => 'admin#create_user',  as: 'create_user'
-
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

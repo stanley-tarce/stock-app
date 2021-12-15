6bb3d209e1a91507c8ccd6fd48cd4ac248caf279
Stanley Tarce <stanleytarce18@gmail.com>
2021-12-15 16:22:01 +0800

    Added Admin Auth

---
+++ app/models/admin.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Admin < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  extend Devise::Models #added this line to extend devise model
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
+end

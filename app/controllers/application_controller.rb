f209cb96e91661fc7dd73f1ee57dd44ba1489f0e
leandrajade <leandrajade@LJadePanopio.localdomain>
2021-12-20 16:51:36 +0800

    uncommented private

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 
 
-  # private
+  private
   def authenticate_if_admin!
     current_api_v1_user.user_type == 'admin'
   end

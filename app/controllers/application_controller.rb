9e9ffe2c4a479d6abb3f2f802cdd6b84dbc7a73a
stanley-tarce <stanleytarce18@gmail.com>
2021-12-21 10:27:39 +0800

    Added Updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,9 @@ class ApplicationController < ActionController::API
   def authenticate_if_admin!
     current_api_v1_user.user_type == 'admin'
   end
+  def authenticate_trader_status!
+    current_api_v1_user.trader.status == 'approved'
+  end
 
 
 end

4e09fb5f2de6e92e1ffb9d91cf53b56a97cfbce9
leandrajade <leandrajade@LJadePanopio.localdomain>
2021-12-22 15:22:29 +0800

    add stock rspec model

--- spec/models/stock_spec.rb
+++ spec/models/stock_spec.rb
@@ -3,23 +3,44 @@ require 'rails_helper'
 RSpec.describe Stock, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   context "Data Creation" do
-    before(:each) do
+   
+
+    it "should be able to create a new stock" do 
       @stock = FactoryBot.create(:stock) 
-    end
-    it "1. should be able to create a new stock" do 
       expect(@stock.valid?).to eq(true)
     end
 
-    it "2. should not be able to create a new market without a stock name" do
-      @stock = FactoryBot.build(:market, stock_name: nil)
+    it "should not be able to create without a stock name" do
+      @stock = FactoryBot.build(:stock, stock_name: nil)
       expect(@stock).to be_invalid
       expect(@stock.errors).to be_present
     end
 
-    it "2. should not be able to create a new market without a price per unit name" do
-      @stock = FactoryBot.build(:market, price_per_unit: nil)
+    it "should be invalid without shares" do
+      @stock = FactoryBot.build(:stock, shares: nil)
       expect(@stock).to be_invalid
       expect(@stock.errors).to be_present
+    end
+
+    it "should be invalid without price per unit" do
+      @stock = FactoryBot.build(:stock, price_per_unit: nil)
+      expect(@stock).to be_invalid
+      expect(@stock.errors).to be_present
+    end
+
+    it "should belong to trader" do
+      t = Stock.reflect_on_association(:trader)
+      expect(t.macro).to eq(:belongs_to)
+    end
+
+    it "should belong to market" do
+      t = Stock.reflect_on_association(:market)
+      expect(t.macro).to eq(:belongs_to)
+    end
+
+    it "should create total price" do
+      @stock = FactoryBot.create(:stock, shares: 3, price_per_unit: 4)
+      expect(@stock.total_price).to eq(12)
     end
   end 
 end

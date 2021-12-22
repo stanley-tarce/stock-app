6a18352c03e026be6e227e96df1cde0663d8885c
leandrajade <leandrajade@LJadePanopio.localdomain>
2021-12-22 12:07:52 +0800

    added model market spec

--- spec/models/market_spec.rb
+++ spec/models/market_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Market, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "Data Creation" do
+    before(:each) do
+      @market = FactoryBot.create(:market) 
+    end
+    it "1. should be able to create a new market" do 
+      expect(@market.valid?).to eq(true)
+    end
+
+    it "2. should not be able to create a new market without a stock name" do
+      @market = FactoryBot.build(:market, stock_name: nil)
+      expect(@market).to be_invalid
+      expect(@market.errors).to be_present
+    end
+
+    it "2. should not be able to create a new market without a price per unit name" do
+      @market = FactoryBot.build(:market, price_per_unit: nil)
+      expect(@market).to be_invalid
+      expect(@market.errors).to be_present
+    end
+  end 
 end
